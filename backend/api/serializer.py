ad889de7e92461bb17bbd5615b4a7f5928429bd2
Rizon1326 <bsse1326@iit.du.ac.bd>
2025-10-17 23:14:32 +0600

    Implement user registration and profile management endpoints with JWT authentication

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -17,34 +17,33 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         # ...
 
         return token
-    
-    class RegisterSerializer(serializers.ModelSerializer):
-        password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-        password2 = serializers.CharField(write_only=True, required=True)
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
 
-        class Meta:
-            model = api_models.User
-            fields = ('full_name','email', 'password', 'password2')
+    class Meta:
+        model = api_models.User
+        fields = ('full_name','email', 'password', 'password2')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        return attrs
+
+    def create(self, validated_data):
+        user = api_models.User.objects.create(
+            full_name=validated_data['full_name'],
+            email=validated_data['email'],
+        )
         
-        def validate(self, attrs):
-            if attrs['password'] != attrs['password2']:
-                raise serializers.ValidationError({"password": "Password fields didn't match."})
+        email_username,mobile = user.email.split('@')
+        user.username = email_username
 
-            return attrs
+        user.set_password(validated_data['password'])
+        user.save()
 
-        def create(self, validated_data):
-            user = api_models.User.objects.create(
-                full_name=validated_data['full_name'],
-                email=validated_data['email'],
-            )
-            
-            email_username,mobile = user.email.split('@')
-            user.username = email_username
-
-            user.set_password(validated_data['password'])
-            user.save()
-
-            return user
+        return user   
         
 
 class UserSerializer(serializers.ModelSerializer):
@@ -123,3 +122,4 @@ class AuthorSerializer(serializers.Serializer):
     posts=serializers.IntegerField(default=0)
     likes=serializers.IntegerField(default=0)
     bookmark=serializers.IntegerField(default=0)
+
